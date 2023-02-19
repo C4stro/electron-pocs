af529c74437722b535d84da61dd780bb4453a6a4
C4stro <matheus.hcastro@live.com>
2023-02-19 02:12:19 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    center: true,
+    backgroundColor: 'gray',
+    minimizable: false,
+    maximizable: false,
+  })
+
+  win.loadFile(path.join(__dirname, 'index.html'))
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
